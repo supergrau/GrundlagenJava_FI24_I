a21b75babfd52733996c697555c7ce2519d214e9
supergrau <mile@lincont.de>
2025-04-09 11:15:16 +0200

    Update Application.java

--- src/main/java/com/futuretrainings/jg/Application.java
+++ src/main/java/com/futuretrainings/jg/Application.java
@@ -1,5 +1,13 @@
 package com.futuretrainings.jg;
 
+/*
+    Bezeichner
+    Groß- und Kleinschreibung
+    reservierte Schlüsselwörter
+    Modifizierer
+    Einrückung
+
+ */
 public class Application {
     public static void main(String[] args) {
         System.out.println("This is a application");
