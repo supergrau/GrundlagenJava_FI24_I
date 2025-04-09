5f144c49a43ddd98ad9b1c7314c74dd27995d4f4
supergrau <mile@lincont.de>
2025-04-09 11:56:27 +0200

    Add Comments.java

---
+++ src/main/java/com/futuretrainings/jg/Comments.java
@@ -0,0 +1,39 @@
+package com.futuretrainings.jg;
+
+
+/*
+        Kommentartypen:
+        - Zeilenkommentar
+        - Blockkommentar
+        - Kommentare für JavaDoc
+ */
+
+/**
+ * Sample for comments
+ */
+public class Comments {
+    /**
+     * Startpoint to programm
+     * @param args Commandline arguments
+     */
+    public static void main(String[] args) {
+        System.out.println();
+        sample(1);
+
+    }
+
+
+    /**
+     * <h1>Sample</h1>
+     * <li>item 1</li>
+     * <li>item 2</li>
+     * <li>item 3</li>
+     * <li>item 4</li>
+     *
+     * @param i I
+     */
+    public static void sample(int i) {
+        int j = i;
+    }
+
+}
